d9bf536542dbd56e4511863bce1b3401e8a72c39
JMan200122 <154390103+JMan200122@users.noreply.github.com>
2024-01-11 15:02:21 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,7 +55,7 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("8-bit-space-123218.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn up volume
+ambientSound.player.volume.value = 30; //turn up volume
 
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
