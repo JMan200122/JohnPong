c7835980b816c7f11409221ff0a01cf19e11f6aa
JMan200122 <154390103+JMan200122@users.noreply.github.com>
2024-01-11 14:52:32 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,7 +55,7 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("cloud-world-calamity-170657.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = 20; //turn up volume
+ambientSound.player.volume.value = 20; //turn dowm volume
 
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
